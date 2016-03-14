4502e5d68fad9ec83aa9d8b3371ac40c80660a34
Ben Vincent <vincent@ualberta.ca>
2016-03-14 14:48:44 -0600

    Passed tests

--- sockets.py
+++ sockets.py
@@ -26,6 +26,34 @@ app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
 
+
+def send_all(msg):
+    for client in clients:
+        client.put( msg )
+
+def send_all_json(obj):
+    send_all( json.dumps(obj) )
+
+def flask_post_json():
+    '''Ah the joys of frameworks! They do so much work for you
+       that they get in the way of sane operation!'''
+    if (request.json != None):
+        return request.json
+    elif (request.data != None and request.data != ''):
+        return json.loads(request.data)
+    else:
+        return json.loads(request.form.keys()[0])
+
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
+
 class World:
     def __init__(self):
         self.clear()
@@ -64,35 +92,48 @@ myWorld = World()
 def set_listener( entity, data ):
     ''' do something with the update ! '''
 
+clients = list()
 myWorld.add_set_listener( set_listener )
+
+def read_ws(ws,client):
+    '''A greenlet function that reads from the websocket'''
+    # XXX: TODO IMPLEMENT ME
+    try:
+        while True:
+            msg = ws.receive()
+            print "WS RECV: %s" % msg
+            if (msg is not None):
+                packet = json.loads(msg)
+                send_all_json( packet )
+            else:
+                break
+    except:
+        '''Done'''
 
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
     return redirect("static/index.html")
 
-def read_ws(ws,client):
-    '''A greenlet function that reads from the websocket and updates the world'''
-    # XXX: TODO IMPLEMENT ME
-    return None
-
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn( read_ws, ws, client )
+    try:
+        while True:
+            # block here
+            msg = client.get()
+            ws.send(msg)
+    except Exception as e:# WebSocketError as e:
+        print "WS Error %s" % e
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
 
-
-def flask_post_json():
-    '''Ah the joys of frameworks! They do so much work for you
-       that they get in the way of sane operation!'''
-    if (request.json != None):
-        return request.json
-    elif (request.data != None and request.data != ''):
-        return json.loads(request.data)
-    else:
-        return json.loads(request.form.keys()[0])
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
@@ -124,9 +165,4 @@ def clear():
 
 
 if __name__ == "__main__":
-    ''' This doesn't work well anymore:
-        pip install gunicorn
-        and run
-        gunicorn -k flask_sockets.worker sockets:app
-    '''
     app.run()
